5c3a7018ad5657b5802b896910861ae4d664d221
Lian Salmerón López <lembajosa@gmail.com>
2023-05-26 19:55:34 +0200

    Cambios en la organización de ficheros pt2

--- ofertas_concat.py
+++ ofertas_concat.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from carrefour import scraper_carrefour
 from Dia import scraper_dia
 import pandas as pd
+import pymysql
+import datetime
 
 def main():
     # Llama a la función de scraping de Carrefour y guarda el resultado en un DataFrame
@@ -31,10 +30,34 @@ def main():
     df_dia['precioAnterior'] = df_dia['precioAnterior'].astype(float)
 
     #merge de los dos dataframes
-    df_mergeado = pd.concat([df_carrefour, df_dia], ignore_index=True)
+    df = pd.concat([df_carrefour, df_dia], ignore_index=True)
 
-    # Guarda el DataFrame concatenado en un archivo CSV
-    df_mergeado.to_csv('ofertas/ofertas_concatenadas.csv', sep=';', index=False)
+    conn = pymysql.connect(host='195.235.211.197', user='pc2_grupo3', password='PComputacion.23', database='pc2_grupo3')
+    cursor = conn.cursor()
+
+    sql_obtenerDatos = "SELECT * FROM ofertas"
+
+    cursor.execute(sql_obtenerDatos)
+    resultados = cursor.fetchall()
+
+    for oferta in range(len(df)):
+        sql = "INSERT INTO ofertas (nombreOferta, precioActual, precioAnterior, imagenOferta, urlOferta, categoria, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        nombreOferta = str(df["nombreOferta"][oferta])
+        precioActual = float(df["precioActual"][oferta])
+        precioAnterior = float(df["precioAnterior"][oferta])
+        imagenOferta = str(df["imagenOferta"][oferta])
+        urlOferta = str(df["urlOferta"][oferta])
+        categoria = str(df["categoria"][oferta])
+        fecha_inserccion = datetime.datetime.now()
+
+        cursor.execute(sql, (
+        nombreOferta, precioActual, precioAnterior, imagenOferta, urlOferta, categoria, fecha_inserccion))
+
+        sqlTrasInserccion = "SELECT id FROM ofertas ORDER BY id DESC LIMIT 1"
+        cursor.execute(sqlTrasInserccion)
+        resultadosTrasLaInserccion = cursor.fetchone()
+
+        conn.commit()
 
 if __name__ == '__main__':
     main()
